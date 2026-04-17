8a226a095684958471be5e8c5e8291eada331f6b
EduGomesNascimento <104858010+EduGomesNascimento@users.noreply.github.com>
2026-04-17 14:50:28 -0300

    Update Supabase config with new URL and anonKey

--- static/js/aen-supabase-config.js
+++ static/js/aen-supabase-config.js
@@ -1,5 +1,5 @@
 window.AEN_SUPABASE_CONFIG = Object.freeze({
-  url: "",
-  anonKey: "",
+  url: "https://ihjoxqswmztwhfvmqwfa.supabase.co",
+  anonKey: "sb_publishable_Y_7EokTC8UWSVFfObLTP5A_KRf4wQlu",
   storageKey: "aensystems-gp-auth"
 });
